2ec480b0ffb65bebfd901920c9f595626e48529a
Leo <losoviz@gmail.com>
2019-12-04 19:27:49 +0800

    Fixed signature

--- src/ModuleProcessors/SingleDBObjectModuleProcessorTrait.php
+++ src/ModuleProcessors/SingleDBObjectModuleProcessorTrait.php
@@ -3,7 +3,7 @@ namespace PoP\QueriedObject\ModuleProcessors;
 
 trait SingleDBObjectModuleProcessorTrait
 {
-    public function getDBObjectIDOrIDs($data_properties)
+    public function getDBObjectIDOrIDs(array $module, array &$props, &$data_properties)
     {
         $vars = \PoP\ComponentModel\Engine_Vars::getVars();
         return $vars['routing-state']['queried-object-id'];
