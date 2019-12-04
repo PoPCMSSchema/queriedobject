573ebacb2056ea1d7f767573258d7faf33f27b0e
Leo <losoviz@gmail.com>
2019-12-04 21:49:42 +0800

    Renamed function

--- src/ModuleProcessors/QueriedDBObjectModuleProcessorTrait.php
+++ src/ModuleProcessors/QueriedDBObjectModuleProcessorTrait.php
@@ -3,7 +3,7 @@ namespace PoP\QueriedObject\ModuleProcessors;
 
 trait QueriedDBObjectModuleProcessorTrait
 {
-    protected function getSingleDBObjectID(array $module, array &$props, &$data_properties)
+    protected function getQueriedDBObjectID(array $module, array &$props, &$data_properties)
     {
         $vars = \PoP\ComponentModel\Engine_Vars::getVars();
         return $vars['routing-state']['queried-object-id'];
