d005776cfa3aed7f260e2123be82e887cab50347
Leo <losoviz@gmail.com>
2019-12-04 19:46:12 +0800

    Migrated loading single DBObject to processor

--- src/ModuleProcessors/SingleDBObjectModuleProcessorTrait.php
+++ src/ModuleProcessors/SingleDBObjectModuleProcessorTrait.php
@@ -3,7 +3,7 @@ namespace PoP\QueriedObject\ModuleProcessors;
 
 trait SingleDBObjectModuleProcessorTrait
 {
-    public function getDBObjectIDOrIDs(array $module, array &$props, &$data_properties)
+    protected function getSingleDBObjectID(array $module, array &$props, &$data_properties)
     {
         $vars = \PoP\ComponentModel\Engine_Vars::getVars();
         return $vars['routing-state']['queried-object-id'];
