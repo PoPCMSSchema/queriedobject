ac8c78a6d3d014d1b0773ab6795d90aef277b39a
Leonardo Losoviz <leo@getpop.org>
2020-06-18 14:56:57 +0800

    Created interface QueryableObject

---
+++ src/FieldInterfaces/QueryableObjectFieldInterfaceResolver.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\QueriedObject\FieldInterfaces;
+
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\FieldInterfaceResolvers\AbstractSchemaFieldInterfaceResolver;
+
+class QueryableObjectFieldInterfaceResolver extends AbstractSchemaFieldInterfaceResolver
+{
+    public const NAME = 'QueryableObject';
+
+    public function getInterfaceName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSchemaInterfaceDescription(): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return $translationAPI->__('Entities that can be queried through an URL', 'queriedobject');
+    }
+
+    public static function getFieldNamesToImplement(): array
+    {
+        return [
+            'url',
+            'slug',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'url' => SchemaDefinition::TYPE_URL,
+            'slug' => SchemaDefinition::TYPE_STRING,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'url' => $translationAPI->__('URL to query the object', 'queriedobject'),
+            'slug' => $translationAPI->__('URL\'s slug', 'queriedobject'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+}
