e4193057b78c864b281d16cdce56f4a07636fe91
Leonardo Losoviz <leo@getpop.org>
2020-08-08 16:51:56 +0800

    Include owner in depended migration plugin

--- src/Component.php
+++ src/Component.php
@@ -26,7 +26,7 @@ class Component extends AbstractComponent
     public static function getDependedMigrationPlugins(): array
     {
         return [
-            'migrate-queriedobject',
+            'pop-schema/migrate-queriedobject',
         ];
     }
 
